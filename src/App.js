87227abaf1df26294fae3c76f42228adb4cc9042
ruth <1054051797@qq.com>
2021-09-06 23:26:08 +0800

    without /core/styles

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
-import { createTheme } from "@material-ui/core/styles";
-import { ThemeProvider } from "@material-ui/styles";
 import IndexPage from "./layouts/IndexPage";
 import SyncSetter from "./layouts/SyncSetter";
 import Login from "./layouts/Login";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      // Purple and green play nicely together.
-      main: "#fff",
-    },
-    secondary: {
-      // This is green.A700 as hex.
-      main: "#11cb5f",
-    },
-  },
-});
-
 function WithHeader(props) {
   return (
     <div id="wrapper">
-      <ThemeProvider theme={theme}>
-        <div id="nav-bar">
-          <NavBar />
-        </div>
-        {props.children}
-      </ThemeProvider>
+      <div id="nav-bar">
+        <NavBar />
+      </div>
+      {props.children}
     </div>
   );
 }
@@ -36,25 +19,23 @@ function WithHeader(props) {
 function App() {
   return (
     <div id="wrapper">
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Switch>
-            <Route exact path="/">
-              <WithHeader>
-                <IndexPage />
-              </WithHeader>
-            </Route>
-            <Route path="/upload-manage">
-              <WithHeader>
-                <SyncSetter />
-              </WithHeader>
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-          </Switch>
-        </Router>
-      </ThemeProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <WithHeader>
+              <IndexPage />
+            </WithHeader>
+          </Route>
+          <Route path="/upload-manage">
+            <WithHeader>
+              <SyncSetter />
+            </WithHeader>
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
     // <Router>
     //   <Switch>
